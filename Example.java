4bfef8fd0c479c09f53dd11fb302e78ddf223938
Clowen3094 <kaninia207@gmail.com>
2017-06-18 00:20:36 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,14 @@
+class example{
+int x,y;
+String name;
+
+public static void main(String args[]){
+example pnt = new example();
+System.out.println("pnt is "+pnt.name+""+pnt.x+""+pnt.y);
+}}
+
+
+
+
+//Output  will be: pnt is null 0 0
+// Option (d) will be the solution
